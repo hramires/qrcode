8fdf0e75ac2eb811ca8e20bc4d68848b2cee49d8
hramires <henrique.ramires@acad.pucrs.br>
2019-07-03 22:06:18 -0300

    first commit

---
+++ src/main/java/com/springboot/ibmcloud/IbmcloudApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.ibmcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmcloudApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springboot/ibmcloud/IndexController.java
@@ -0,0 +1,13 @@
+package com.springboot.ibmcloud;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Olá mudo com IBM Cloud";
+	}
+}
